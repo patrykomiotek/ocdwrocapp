20dd62563908796f67c6ff3c5dcb7f518ba3e4f9
Patryk Omiotek <patryk.omiotek@gmail.com>
2025-06-03 23:27:31 +0200

    feat: with tests config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,34 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig as defineViteConfig, mergeConfig } from "vite";
+import {
+  configDefaults,
+  defineConfig as defineVitestConfig,
+} from "vitest/config";
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+const viteConfig = defineViteConfig({
+  plugins: [react(), tsconfigPaths()],
+});
+
+const vitestConfig = defineVitestConfig({
+  test: {
+    environment: "jsdom",
+    globals: true,
+    setupFiles: ["./vitest-setup.ts"],
+    coverage: {
+      reporter: ["text", "json", "html"],
+    },
+    exclude: [
+      ...configDefaults.exclude,
+      "./tests/**",
+      "./tests-examples/**",
+      "./src/stories/**",
+      "./src/**/*.{stories}.tsx",
+      "./src/**/*.{stories}.ts",
+      "./src/ui/utils/cn.ts",
+    ],
+  },
+});
+
+export default mergeConfig(viteConfig, vitestConfig);
