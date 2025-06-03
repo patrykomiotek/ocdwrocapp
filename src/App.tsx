f5225d3a783ec0a59dcc5d5ebb37757416b11676
Patryk Omiotek <patryk.omiotek@gmail.com>
2025-06-03 12:02:31 +0200

    feat: with barrels

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import { Text } from "./ui/Text";
+import { Button, Text } from "./ui";
+
 import "./App.css";
-import { Button } from "./ui/Button";
 
 function App() {
   const [count, setCount] = useState(0);
