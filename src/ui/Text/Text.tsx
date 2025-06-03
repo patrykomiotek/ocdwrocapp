620820e18d507924b04de003501c6ba8e04dda4c
Patryk Omiotek <patryk.omiotek@gmail.com>
2025-06-03 16:16:59 +0200

    fix: types

--- src/ui/Text/Text.tsx
+++ src/ui/Text/Text.tsx
@@ -6,7 +6,7 @@ import { memo } from "react";
 import type { PropsWithChildren } from "react";
 
 interface Props {
-  children: string | string[] | number | number[]; // React.ReactNode
+  children: string | number | (string | number)[]; // React.ReactNode
 }
 
 // export function Text(props: { children: string }) {
