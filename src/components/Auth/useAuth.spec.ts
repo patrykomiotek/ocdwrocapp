fa1e732fef3161967a00a5757d71de60fa6f6add
Patryk Omiotek <patryk.omiotek@gmail.com>
2025-06-04 11:51:44 +0200

    feat: with useAuth tests

--- src/components/Auth/useAuth.spec.ts
+++ src/components/Auth/useAuth.spec.ts
@@ -1,5 +1,57 @@
+import { renderHook, act } from '@testing-library/react';
 import { useAuth } from './AuthContext';
 
 describe('useAuth hook', () => {
-  it('should toggle values', () => {});
+  it('should toggle values', () => {
+    const { result } = renderHook(() => useAuth());
+
+    expect(result.current.isLoggedIn).toBe(false);
+
+    act(() => {
+      result.current.toggleLoggedIn();
+    });
+    // waitFor()
+
+    expect(result.current.isLoggedIn).toBe(true);
+  });
+
+  it('should set value to true', () => {
+    const { result } = renderHook(() => useAuth());
+
+    expect(result.current.isLoggedIn).toBe(false);
+
+    act(() => {
+      result.current.logIn();
+    });
+    // waitFor()
+
+    expect(result.current.isLoggedIn).toBeTruthy();
+  });
+
+  it('should login', () => {
+    const { result } = renderHook(() => useAuth());
+
+    expect(result.current.isLoggedIn).toBe(false);
+
+    act(() => {
+      result.current.logIn();
+    });
+    // waitFor()
+
+    expect(result.current.isLoggedIn).toBeTruthy();
+  });
+
+  it('should logout', () => {
+    const { result } = renderHook(() => useAuth());
+
+    expect(result.current.isLoggedIn).toBe(false);
+
+    act(() => {
+      result.current.logIn();
+      result.current.logOut();
+    });
+    // waitFor()
+
+    expect(result.current.isLoggedIn).toBeFalsy();
+  });
 });
