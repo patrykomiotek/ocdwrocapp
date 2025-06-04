a293e521871c838c53b267895ff2625d02eb9859
Patryk Omiotek <patryk.omiotek@gmail.com>
2025-06-04 15:43:52 +0200

    feat: with mocking

--- src/components/Generator/Generator.spec.tsx
+++ src/components/Generator/Generator.spec.tsx
@@ -1,16 +1,32 @@
+import { vi } from 'vitest';
 import { render, screen, fireEvent } from '@testing-library/react';
 import { Generator } from './Generator';
 
+vi.mock('uuid', () => ({
+  v4: vi
+    .fn()
+    .mockImplementationOnce(() => 'kaczka')
+    .mockImplementationOnce(() => 'pomidor'),
+}));
+
+// afterEach(() => {
+//   vi.clearAllMocks();
+// });
+
 describe('Generator component', () => {
   it('should generate new values', () => {
-    render(<Generator />);
-    const first = screen.getByText(/^[0-9a-fA-F-]{36}$/).textContent;
+    const { debug } = render(<Generator />);
+    // const first = screen.getByText(/^[0-9a-fA-F-]{36}$/).textContent;
+    // const first = screen.getByText('kaczka').textContent;
+
+    expect(screen.getByText('kaczka')).toBeInTheDocument();
 
     // fireEvent.click(screen.getByRole('button', { name: /regenerate/i }));
     fireEvent.click(screen.getByRole('button'));
 
-    const second = screen.getByText(/^[0-9a-fA-F-]{36}$/).textContent;
-
-    expect(second).not.toBe(first);
+    // const second = screen.getByText(/^[0-9a-fA-F-]{36}$/).textContent;
+    debug();
+    // expect(second).not.toBe(first);
+    expect(screen.getByText('pomidor')).toBeInTheDocument();
   });
 });
