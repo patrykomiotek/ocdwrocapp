ad8722dd8478f092b3c180cac88a9c4d6a5c3b15
Patryk Omiotek <patryk.omiotek@gmail.com>
2025-06-04 10:21:13 +0200

    feat: auth compoentns

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,19 @@ import './App.css';
 import { Heading } from 'ui/Heading';
 import { AuthContextPage } from './pages/AuthContextPage';
 import { AuthContext } from './components/Auth/AuthContext';
+import { useState } from 'react';
 
 function App() {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   return (
     <div>
       <Heading variant="h1" className="text-blue-600">
         Blue header
       </Heading>
 
-      <AuthContext value={{ isLoggedIn: true }}>
+      <AuthContext value={{ isLoggedIn }}>
         <AuthContextPage />
         {/* <ViewportPage /> */}
         {/* <CounterPage /> */}
