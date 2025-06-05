a35a4cb025a79b9f9cb678b6f9ac92413f3c5c69
Patryk Omiotek <patryk.omiotek@gmail.com>
2025-06-05 15:16:23 +0200

    feat: tests scenarios

--- src/pages/ProductListPage.spec.tsx
+++ src/pages/ProductListPage.spec.tsx
@@ -2,4 +2,6 @@ describe('ProductListPage component', () => {
   it.todo('should display loading indicator');
   it.todo('should display error indicator');
   it.todo('should display at least two products');
+
+  it.todo('should display two different products after refetch');
 });
