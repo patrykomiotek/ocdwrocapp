37d8e7c4647291a1fc1a9893dd72f1dcaa15bf1a
Patryk Omiotek <patryk.omiotek@gmail.com>
2025-06-05 15:15:18 +0200

    feat: test placeholders

---
+++ src/pages/ProductListPage.spec.tsx
@@ -0,0 +1,5 @@
+describe('ProductListPage component', () => {
+  it.todo('should display loading indicator');
+  it.todo('should display error indicator');
+  it.todo('should display at least two products');
+});
